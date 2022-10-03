941efd6f1699dfe0738415e888344c2d15b48fa3
Angel Zhou <angel.zhou1016@gmail.com>
2022-10-03 17:50:58 -0400

    imports snack model

--- server.js
+++ server.js
@@ -5,11 +5,26 @@ const morgan = require("morgan")
 const mongoose = require("mongoose") 
 const path = require("path") 
 
+const Snack = require('./models/snack')
+
 ///// Database connection //////
 const DATABASE_URL = process.env.DATABASE_URL
 const CONFIG = {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }
-
 mongoose.connect(DATABASE_URL, CONFIG)
+mongoose.connection
+    .on('open', () => console.log('Connected to Mongoose'))
+    .on('close', () => console.log('Disconnected from Mongoose'))
+    .on('error', (error) => console.log('An error occurred: \n', error))
+
+
+/////// Express App Object ////////
+const app = express()
+
+///// Middleware ///////
+app.use(morgan('tiny'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public'))
+app.use(express.json())
