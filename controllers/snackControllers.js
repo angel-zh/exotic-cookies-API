a41725079aed4c04952baf13ee570f889cbf5f3f
Angel Zhou <angel.zhou1016@gmail.com>
2022-10-04 18:52:07 -0400

    established routes in separate file

---
+++ controllers/snackControllers.js
@@ -0,0 +1,65 @@
+
+// Import Dependencies
+const express = require('express')
+const Snack = require('../models/snack')
+
+
+// Create Router
+const router = express.Router()
+
+// Routes
+// Index Route
+router.get('/', (req, res) => {
+    Snack.find({})
+        .then(snacks=> {
+            res.json({ snacks: snacks})
+    })
+    .catch(err => console.log(err))
+})
+
+
+// Show Route
+arouterpp.get('/:id', (req, res) => {
+    const id = req.params.id
+    Snack.findById(id)
+        .then(snack =>  {
+            res.json({ snack: snack })
+        })
+        .catch(err => console.log(err))
+})
+
+
+// Create route
+router.post('/', (req, res) => {
+    Snack.create(req.body)
+        .then(snack => {
+            res.status(201).json({ snack: snack.toObject() })
+        })
+        .catch(err => console.log(err))
+})
+
+
+// Update Route
+router.put('/:id', (req, res) => {
+    const id = req.params.id
+    Snack.findByIdAndUpdate(id, req.body, {new: true})
+        .then(snack => {
+            res.sendStatus(204)
+        })
+        .catch(err => res.json(err))
+})
+
+
+// Delete Route
+router.delete('/:id', (req, res) => {
+    const id = req.params.id
+    Snack.findByIdAndRemove(id)
+        .then(snack =>  {
+            res.sendStatus(204)
+    })
+        .catch(err => console.log(err))
+})
+
+
+// Export the Router
+module.exports = router
