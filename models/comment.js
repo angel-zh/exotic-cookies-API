bac82d62277483d2342ff82ac6e1bb01e6e84d22
Angel Zhou <angel.zhou1016@gmail.com>
2022-10-05 16:57:01 -0400

    adds the comment subdocument

---
+++ models/comment.js
@@ -0,0 +1,23 @@
+// we are making a subdocument here
+
+// Import Dependencies
+const mongoose = require('./connection')
+const { Schema } = mongoose
+
+// comment schema
+const commentSchema = new Schema({
+    note: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    }
+}, {
+    timestamps: true
+})
+
+// Export our schema
+module.exports = commentSchema
