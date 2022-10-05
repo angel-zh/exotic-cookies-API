d0bab66750c1390ef63e08540d0d032375df4264
Angel Zhou <angel.zhou1016@gmail.com>
2022-10-05 18:10:22 -0400

    register routes for comments and ratings

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const express = require("express")
 
 const path = require("path") 
 const CookieRouter = require('./controllers/CookieControllers')
+const CommentRouter = require('./controllers/commentControllers')
+const RatingRouter = require('./controllers/ratingControllers')
 const UserRouter = require('./controllers/userControllers')
 const middleware = require('./utils/middleware')
 
@@ -20,9 +22,12 @@ middleware(app)
 app.get('/', (req, res) => {
     res.send(`<body style="background-color:pink;text-align:center"><h1>Welcome to the Exotic Cookies API homepage!</h1><p>Here you will find some information about unique cookie flavors. Enjoy!</p></body>`)
 })
-// Register our Routs
+// Register our Routes
+// first argument is base url endpoint, second is the file to use
 app.use('/cookiess', CookieRouter)
+app.use('/comments', CommentRouter)
 app.use('/users', UserRouter)
+app.use('/ratings', RatingRouter)
 
 ////// Server Listener ///////
 const PORT = process.env.PORT
